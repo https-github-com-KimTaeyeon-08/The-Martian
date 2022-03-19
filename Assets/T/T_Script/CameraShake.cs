d6bf1ffebb9df3ea27c8c6600b7a53888b9799ae
KimTaeyeon-08 <ssg9732@gmail.com>
2022-03-19 18:16:09 +0900

    upload start, load scene
    Former-commit-id: 7eb231c454caa4f0553418d62519c1b9126122d8

---
+++ Assets/T/T_Script/CameraShake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float power;
+    public float shake_time;
+   
+    Vector3 position;
+   public void shakefortime(float time)
+    {
+        shake_time = time;
+    }
+    private void Start()
+    {
+        position = new Vector3(980f, 547.3f, -583f);
+    }
+    private void Update()
+    {
+        if (shake_time > 0)
+        {
+            transform.position = Random.insideUnitSphere * power + position;
+            
+            shake_time -= Time.deltaTime;
+        }
+        else
+        {
+            shake_time = 0.0f;
+            transform.position = position;
+        }
+    }
+}
---
+++ Assets/T/T_Script/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnclickLanding()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+    public void OnclickStart()
+    {
+        SceneManager.LoadScene("LoadScene");
+    }
+    public void OnclickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
